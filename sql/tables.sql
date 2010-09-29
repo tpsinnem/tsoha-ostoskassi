b51b2bb1b552a0b3c453b442bea463b9e272fb40
Timo Sinnemäki <tsinnema@tktl-2009.(none)>
2010-09-29 13:49:20 +0300

    create tablet kirjoitettu. ei testattu.

---
+++ sql/tables.sql
@@ -0,0 +1,43 @@
+create table tilaukset (
+   id       serial   PRIMARY KEY,
+   henkilo  integer,
+   lento    integer,
+   tuote    integer  REFERENCES tuotteet(id),
+   kpl      integer  NOT NULL,
+
+   FOREIGN KEY (henkilo, lento) REFERENCES paikkavaraukset
+);
+
+create table paikkavaraukset (
+   henkilo  integer  REFERENCES henkilot (id),
+   lento    char(10) REFERENCES lennot(tunnus),
+   paikka   char(10) NOT NULL,
+
+   PRIMARY KEY (henkilo, lento),
+   UNIQUE      (lento, paikka)
+);
+
+create table lennot (
+   tunnus   char(10) PRIMARY KEY
+);
+
+create table tuotteet (
+   id          integer     PRIMARY KEY,
+   nimi        varchar(50) NOT NULL,
+   hinta       integer     NOT NULL,
+   esittely    text,
+   tuoteryhma  integer     REFERENCES tuoteryhmat(id)
+);
+
+create table henkilot (
+   id          integer     PRIMARY KEY,
+   nimi        varchar(50) NOT NULL,
+   tunnus      char(8)     UNIQUE NOT NULL,
+   salasana    varchar(50) NOT NULL,
+   yllapitaja  boolean     NOT NULL
+);
+
+create table tuoteryhmat (
+   id    integer     PRIMARY KEY,
+   nimi  varchar(20) NOT NULL
+);
