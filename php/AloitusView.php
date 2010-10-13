d82505aa1f5560c6876799752655f60fc26f1d5a
Timo Sinnemäki <tsinnema@tktl-2009.(none)>
2010-10-13 17:54:14 +0300

    dtd, htmlt, headit ja bodyt lisätty

--- php/AloitusView.php
+++ php/AloitusView.php
@@ -5,7 +5,9 @@ include_once "include.php";
 class AloitusView extends View {
 
    public static function tulosta() {
+      parent::alku();
       parent::valikko();
+      parent::loppu();
    }
 }
 
--- php/View.php
+++ php/View.php
@@ -2,6 +2,33 @@
 
 class View {
 
+   public static function alku() {
+      ?>
+
+      <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0//EN"
+              "http://www.w3.org/TR/html4/strict.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml">
+      <head>
+         <title>Tax-free -ostoskassi</title>
+         <meta http-equiv="content-type" 
+            content="text/html;charset=utf-8" />
+      </head>
+      <body>
+
+      <?php
+   }
+
+   public static function loppu() {
+      ?>
+
+      </body>
+      </html>
+
+      <?php
+   }
+
+
+
    public static function valikko() {
       // TÄYDENNÄ
       echo('<div class=\"valikko\">');
