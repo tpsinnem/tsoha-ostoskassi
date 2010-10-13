710fd26a3b56b681efd65f23a6f5d2b5a7636be8
Timo Sinnemäki <tsinnema@tktl-2009.(none)>
2010-10-13 17:18:59 +0300

    korjattu väärin muistettu luokkametodikutsusyntaksi

--- php/View.php
+++ php/View.php
@@ -5,9 +5,9 @@ class View {
    public static function valikko() {
       // TÄYDENNÄ
       echo('<div class=\"valikko\">');
-      echo('<a href=\"'.Controller.url('tuott').'\">Tuotteet</a>');
+      echo('<a href=\"'.Controller::url('tuott').'\">Tuotteet</a>');
       if (!isset($_SESSION['tunnus'])) {
-         echo('<a href=\"'.Controller.url('akirj').'\">Kirjaudu</a>');
+         echo('<a href=\"'.Controller::url('akirj').'\">Kirjaudu</a>');
       }
       echo('</div>');
    }
--- php/index.php
+++ php/index.php
@@ -2,6 +2,6 @@
 
 include_once "include.php";
 
-Controller.aja();
+Controller::aja();
 
 ?>
