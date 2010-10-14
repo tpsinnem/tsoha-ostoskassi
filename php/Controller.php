1a156fd8998ca8abf70ce1c4d1426009cd4e8821
Timo Sinnemäki <tsinnema@tktl-2009.(none)>
2010-10-14 16:49:51 +0300

    urlien muodostuksen säätöä

--- php/Controller.php
+++ php/Controller.php
@@ -4,12 +4,16 @@ include_once "include.php";
 
 class Controller {
 
+   const KANTA_URL = 'http://tsinnema.users.cs.helsinki.fi/tsoha';
+
+/*
    private static function nykyinenUrl() {
       return "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
    }
    private static function korvaaSivu($termi) {
-      return preg_replace("/sivu=[[:alnum:]]*/", "sivu=$termi", self::nykyinenUrl());
+      return preg_replace("#sivu=[[:alnum:]]*#", 'sivu=$termi', self::nykyinenUrl());
    }
+*/
 
    public static function aja() {
       //TÄYDENNÄ
@@ -21,11 +25,29 @@ class Controller {
    }
 
    public static function url($termi) {
-      $url = "";
+      $parametrit = null;
+      foreach ($_GET as $kentta => $arvo) {
+         $parametrit[$kentta] = $arvo;
+      }
+
       if (  $termi == 'tuott' ||
             $termi == 'akirj') {
-         $url = self::korvaaSivu($termi);
+         $parametrit['sivu'] = $termi;
+         //$url = self::korvaaSivu($termi);
       }
+
+      $parametriString = ""
+      foreach ($parametrit as $kentta => $arvo) {
+         if ($parametriString === "") {
+            $parametriString .= '?';
+         } else {
+            $parametriString .= '&';
+         }
+         $parametriString .= '$kentta=$arvo';
+      }
+
+
+      $url = KANTA_URL.$parametriString;
       return $url;
    }
 }
