34969fa506cc05a1e661bc2907a0bdd302c2e37b
Timo Sinnemäki <timo.sinnemaki@helsinki.fi>
2010-10-14 17:30:40 +0300

    kirjautumisen 1. versio

---
+++ php/AsiakasKirjautumisView.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once "include.php";
+
+class AsiakasKirjautumisView extends View {
+
+   public static function tulosta() {
+      parent::alku();
+      parent::valikko();
+      
+      ?>
+      <div class="kirjautuminen">
+      <form action="<?php echo(Controller::url('aloit')); ?>" method="post">
+      <p>
+         <label for="tunnus">Tunnus: </label>
+            <input type="text" id="tunnus" /><br />
+         <label for="salasana">Salasana: </label>
+            <input type="password" id="salasana" /><br />
+         <input type="submit" value="Kirjaudu" />
+      </p>
+      </form>
+      </div>
+      <?php
+
+      parent::loppu();
+   }
+}
+
+?>
