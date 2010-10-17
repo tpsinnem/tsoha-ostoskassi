818a10bc322ef33db3096b823c725f2c2031462a
Timo Sinnemäki <timo.sinnemaki@helsinki.fi>
2010-10-17 05:25:52 +0300

    tilaukset

--- php/TilauksetView.php
+++ php/TilauksetView.php
@@ -29,8 +29,8 @@ class TilauksetView extends View {
             echo("</dd>\n");
          }
          echo("</dl>\n");
-         if (  !isset($_SESSION['yllapitaja']) ||
-               $_SESSION['yllapitaja'] == false) {
+         if (  (!isset($_SESSION['yllapitaja'])) ||
+               ($_SESSION['yllapitaja'] == false)  ) {
             echo('<p><a href="'.
                   Controller::url('tuotteet').
                   '">Tee uusi tilaus</a></p>'."\n");
