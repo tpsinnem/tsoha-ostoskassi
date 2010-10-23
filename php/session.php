737b6b0d00fb1e82c89fe378f1b854da972ebbac
Timo Sinnemäki <timo.sinnemaki@helsinki.fi>
2010-10-23 10:26:38 +0300

    tilauksen  muokkauksen korjailua

--- php/session.php
+++ php/session.php
@@ -1,6 +1,6 @@
 <?php
 session_save_path('/home/tsinnema/tsoha-ostoskassi/session/');
-session_name('tyoaika');
+session_name('tsoha-ostoskassi');
 session_set_cookie_params(0, '/tsoha');
 session_start();
 ?>
